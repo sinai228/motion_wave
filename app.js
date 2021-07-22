57fcbb63294fbdce1831b0a14c7efbe638439afd
sinai228 <59324766+sinai228@users.noreply.github.com>
2021-07-22 13:24:22 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import {
-  WaveGroup
-} from './wavegroup.js';
+  Wave
+} from './wave.js';
 
 class App {
 constructor() {
@@ -8,7 +8,7 @@ this.canvas = document.createElement('canvas');
 this.ctx = this.canvas.getContext('2d');
 document.body.appendChild(this.canvas);
   
-  this.waveGroup = new WaveGroup();
+  this.wave = new Wave();
   
   window.addEventListener('resize', this.resize.bind(this), false);
   this.resize();
@@ -24,13 +24,13 @@ this.canvas.width = this.stageWidth * 2;
 this.canvas.height = this.stageHeight * 2;
 this.ctx.scale(2, 2);
   
-  this.waveGroup.resize(this.stageWidth, this.stageHeight);
+  this.wave.resize(this.stageWidth, this.stageHeight);
 } 
 
 animate(t) {
 this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
   
-  this.waveGroup.draw(this.ctx);
+  this.wave.draw(this.ctx);
   
 requestAnimationFrame(this.animate.bind(this));
 }}
