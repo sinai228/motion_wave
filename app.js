3763042a8f53ceee6f69712693a7610c1226ab98
sinai228 <59324766+sinai228@users.noreply.github.com>
2021-07-22 12:53:56 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+import {
+  Wave 
+} from './wave.js';
+
 class App {
 constructor() {
 this.canvas = document.createElement('canvas');
 this.ctx = this.canvas.getContext('2d');
 document.body.appendChild(this.canvas);
+  
+  this.wave = new Wave();
   
   window.addEventListener('resize', this.resize.bind(this), false);
   this.resize();
@@ -12,16 +18,21 @@ document.body.appendChild(this.canvas);
 
 resize() {
 this.stageWidth = document.body.clientWidth;
-this.stageHeight = docuent.body.clientHeight;
+this.stageHeight = document.body.clientHeight;
 
 this.canvas.width = this.stageWidth * 2;
 this.canvas.height = this.stageHeight * 2;
-this.ctx.sclae(2, 2);
+this.ctx.scale(2, 2);
+  
+  this.wave.resize(this.stageWidth, this.stageHeight);
 } 
 
 animate(t) {
-this.ctx.cleaerRect(0, 0, this.stageWidth, this.stageHeight);
-requestAnimationFrame(This.animate.bind(this));
+this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+  
+  this.wave.draw(this.ctx);
+  
+requestAnimationFrame(this.animate.bind(this));
 }}
 
 window.onload = () => {
